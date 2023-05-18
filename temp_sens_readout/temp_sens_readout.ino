7dccf0d536bf792bf9989b77ec7eaa7f1fe2d43e
Anders <anders556@live.no>
2023-05-18 12:26:18 +0200

    Finished writing the test code for the temperature readout
    Signed-off-by: Anders <anders556@live.no>

--- temp_sens_readout/temp_sens_readout.ino
+++ temp_sens_readout/temp_sens_readout.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
 
+// File based on the tutorial: https://randomnerdtutorials.com/arduino-k-type-thermocouple-max6675/
+#include "max6675.h"
+
+int thermoDO = 4;
+int thermoCS = 5;
+int thermoCLK = 6;
+
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("MAX6675 test");
+  // waiting for the MAX chip to stabilize
+  delay(500); // Might need to bake this into the final program.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  Serial.print("C = ");
+  Serial.println(thermocouple.readCelsius());
+  // there must be a delay of atleast 250ms between each read.
+  delay(500); // delay uses milliseconds as the understood input magnitude.
+
+
 
 }
