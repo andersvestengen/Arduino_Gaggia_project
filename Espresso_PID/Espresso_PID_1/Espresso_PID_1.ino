864914053aed6fc03f76348f42327fba98c77ebe
Anders G Vestengen <a.g.vestengen@fys.uio.no>
2023-06-14 20:59:06 +0200

    Fixed most of the compiler errors. Small scope error still remaining
    Signed-off-by: Anders G Vestengen <a.g.vestengen@fys.uio.no>

--- Espresso_PID/Espresso_PID_1/Espresso_PID_1.ino
+++ Espresso_PID/Espresso_PID_1/Espresso_PID_1.ino
@@ -13,10 +13,10 @@ MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 // Dimmer setup
 #include <RBDdimmer.h>
 
-int ZeroCross_Pin = 14; // A0 pin
-int DimmerDIM = 11;
-int DimmerZC = 12;
-dimmerLamp dimmer(11, 12);
+#define ZeroCross_Pin 14 // A0 pin
+#define DimmerDIM 11
+#define DimmerZC 12
+dimmerLamp dimmer(DimmerDIM);
 
 // Pressure sensor setup
 int PressureAnalogIn = A1;
@@ -30,14 +30,15 @@ PID PressurePID(&input, &output, &setpoint, 2, 5, 1, DIRECT);
 
 // setup for the temperature PID loop
 // We can assume SSR is normally off 
-double, Tinput, Toutput, Tsetpoint;
+double Tinput, Toutput, Tsetpoint;
 PID TemperaturePID(&Tinput, &Toutput, &Tsetpoint, 2, 5, 1, DIRECT);
 int WindowSize = 3000; // 3s wait between relay on/off
 unsigned long windowSTartTime;
 
 // SSR Relay setup
 #define RelayPin 2
-
+int PressureReading;
+double CurrentPressure;
 
 void setup() {
   //Setup writing to console for testing. TBD
@@ -47,20 +48,18 @@ void setup() {
   dimmer.toggleSettings(0,100); // sets the dimmermode operating range when (smooth) toggling mode is enabled
   dimmer.begin(NORMAL_MODE, ON); // toggles dimmer on or off  
   pinMode(ZeroCross_Pin, INPUT);
-  int PressureReading;
-  double CurrentPressure;
 
   // Define and initialize Pressure PID values
   setpoint = 9;
-  PressurePID.setMode(AUTOMATIC)
-  Pressure.setOutputLimits(0, 100);
+  PressurePID.SetMode(AUTOMATIC);
+  PressurePID.SetOutputLimits(0, 100);
 
   // Define and Initalize Temperature PID values
   Tsetpoint = 93;
   pinMode(RelayPin, OUTPUT);
   windowStartTime = millis();
   TemperaturePID.SetOutputLimits(0, WindowSize);
-  TemperaturePID.SetMode(AUTOMATIC)
+  TemperaturePID.SetMode(AUTOMATIC);
 
 }
 
@@ -68,7 +67,7 @@ void loop() {
 
 
   //feed octo output to dimmer ZC pin.
-  digitalWrite(DimmerZC, digitalRead(ZeroCross_PIN)); // feed octo zero-cross events to the dimmer ZC input
+  digitalRead(DimmerZC); // feed octo zero-cross events to the dimmer ZC input
   
   // Calculate current pressure
   PressureReading = analogRead(PressureAnalogIn);
@@ -76,13 +75,13 @@ void loop() {
 
   //Run PID calc for pressure loop
   input = CurrentPressure;
-  PressurePID.compute();
+  PressurePID.Compute();
 
   //Output PID proportion to Dimmer
   dimmer.setPower(int(output));
 
   // Read temperature and compute PID
-  Tinput (double) thermocouple.readCelsius();
+  double Tinput = thermocouple.readCelsius();
   TemperaturePID.Compute();
   unsigned long now = millis();
   if (now - windowStartTime > WindowSize)
@@ -106,10 +105,10 @@ double PressureCalc(int Vsig){
   int Vmin = 0.5;
   int Vmax = 4; // this is Vmax - Vmin 
   int MaxBar = 12;
-  int ADC = 1024;
+  int ADC_int = 1024;
 
 
-  pressure = MaxBar * (Vsig*ADC - Vmin) / Vmax;
+  pressure = MaxBar * (Vsig*ADC_int - Vmin) / Vmax;
 
   return pressure;
 
